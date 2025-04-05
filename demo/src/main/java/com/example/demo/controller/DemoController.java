06f92adc46153c838bec212ef6e245aec3699119
Padma Naresh <padmanaresh1986@gmail.com>
2025-04-05 14:25:45 +0530

    dummy commit

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -9,7 +9,8 @@ public class DemoController {
     @GetMapping("/greet")
     public String greet(){
         String unused_variable = "Test";
-        String unused_variable1 = "Test";        
+        String unused_variable1 = "Test";   
+        String unused_variable2 = "Test";      
         return "hello world";
     }
 }
